178aea778d297402ae683d07325989a2e62a4705
Mike Fossel <mikefossel@gmail.com>
2016-02-03 16:52:25 -0800

    first commit

---
+++ MovieNerd.WEb/js/app.js
@@ -0,0 +1,16 @@
+﻿angular.module('app', []);
+angular.module('app').controller('indexController', function ($scope, $http) {
+
+    $scope.searchitem = '';
+
+    $scope.search = function () {
+        $http.get('http://www.omdbapi.com/?s=' + $scope.searchitem)
+               .success(function (data) {
+                $scope.movies = data.Search;
+               })
+               .error(function (error) {
+               });
+                                          }
+   
+});
+
